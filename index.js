bd3a775f505c80bc922130ccf9989e8a1a559ba4
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-06-29 12:09:00 +0300

    fix re-export

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as TicTacToe } from 'examples/react/modules/tic-tac-toe/game';
-export { default as Chess } from 'examples/react/modules/chess/game';
+export { default as TicTacToe } from './examples/react/modules/tic-tac-toe/game';
+export { default as Chess } from './examples/react/modules/chess/game';
