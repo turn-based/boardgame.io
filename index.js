925e5b8a55e62cb167422bb2fae60810206791a2
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-07-02 08:17:54 +0300

    export secret state

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as TicTacToe } from './examples/react/modules/tic-tac-toe/game';
 export { default as Chess } from './examples/react/modules/chess/game';
+export { default as SecretState } from './examples/react/modules/secret-state/game';
